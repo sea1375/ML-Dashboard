134d5401f855d459fbee7f83189bf4533e3ed18b
Sea1375 <sea.dream0000@gmail.com>
2021-02-02 06:51:12 -0800

    anychart based on data set

--- project/app/base/static/assets/js/antchart.js
+++ project/app/base/static/assets/js/antchart.js
@@ -1,28 +1,30 @@
-anychart.onDocumentReady(function() {
-    anychart.data.loadJsonFile("https://static.anychart.com/git-storage/word-press/data/network-graph-tutorial/data.json", function(data) {
-        // create chart from loaded data
-        console.log(data)
-        var chart = anychart.graph(data);
-        // set title
-        // chart.title("Network Graph showing the battles in Game of Thrones");
-        // access nodes
-        var nodes = chart.nodes();
+var data = {
+    nodes: [
+        { id: "Richard" },
+        { id: "Larry" },
+        { id: "Marta" },
+        { id: "Jane" },
+        { id: "Norma" },
+        { id: "Frank" },
+        { id: "Brett" }
+    ],
+    edges: [
+        { from: "Richard", to: "Larry" },
+        { from: "Richard", to: "Marta" },
+        { from: "Larry", to: "Marta" },
+        { from: "Marta", to: "Jane" },
+        { from: "Jane", to: "Norma" },
+        { from: "Jane", to: "Frank" },
+        { from: "Jane", to: "Brett" },
+        { from: "Brett", to: "Frank" }
+    ]
+};
 
-        // set the size of nodes
-        nodes.normal().height(5);
-        // nodes.hovered().height(45);
-        // nodes.selected().height(45);
+// create a chart and set the data
+var chart = anychart.graph(data);
 
-        // set the fill of nodes
-        nodes.normal().fill("#ffa000");
-        // nodes.hovered().fill("white");
-        // nodes.selected().fill("#ffa000");
+// set the container id
+chart.container("wait1");
 
-        // set the stroke of nodes
-        nodes.normal().stroke(null);
-        // nodes.hovered().stroke("#333333", 3);
-        // nodes.selected().stroke("#333333", 3);
-        // draw chart
-        chart.container("wait1").draw();
-    });
-});
+// initiate drawing the chart
+chart.draw();
--- project/app/base/static/assets/js/openfile.js
+++ project/app/base/static/assets/js/openfile.js
@@ -10,9 +10,7 @@ document.querySelector('#selectBtn').addEventListener('click', function (event) 
     });
 }); */
 
-
-
-function handleFileSelect (e) {
+function handleFileSelect(e) {
     var files = e.target.files;
     if (files.length < 1) {
         return;
@@ -21,29 +19,33 @@ function handleFileSelect (e) {
     var reader = new FileReader();
     /* reader.onload = onFileLoaded; */
     /* reader.readAsDataURL(file); */
-	reader.addEventListener("load", e => {
+    reader.addEventListener("load", e => {
         /* console.log(e.target.result, JSON.parse(reader.result)) */
         var dataFromChunkG = JSON.parse(reader.result);
-        var numberOfNodes = 0, numberOfServices = 0, numberOfPods = 0,
-            numberOfExternalIPs = 0, nubmerOfWorkers = 0, numberOfManagement = 0;
+        var numberOfNodes = 0,
+            numberOfServices = 0,
+            numberOfPods = 0,
+            numberOfExternalIPs = 0,
+            nubmerOfWorkers = 0,
+            numberOfManagement = 0;
 
         numberOfNodes = dataFromChunkG.nodes.length;
-        for(let i = 0; i < numberOfNodes; i++) {
-            switch(dataFromChunkG.nodes[i].typ) {
+        for (let i = 0; i < numberOfNodes; i++) {
+            switch (dataFromChunkG.nodes[i].typ) {
                 case 'SVC':
-                    numberOfServices ++;
+                    numberOfServices++;
                     break;
                 case 'POD':
-                    numberOfPods ++;
+                    numberOfPods++;
                     break;
                 case 'EXT':
-                    numberOfExternalIPs ++;
+                    numberOfExternalIPs++;
                     break;
                 case 'WHOST':
-                    nubmerOfWorkers ++;
+                    nubmerOfWorkers++;
                     break;
                 case 'MHOST':
-                    numberOfManagement ++;
+                    numberOfManagement++;
                     break;
             }
         }
@@ -53,17 +55,17 @@ function handleFileSelect (e) {
         document.getElementById('ips').innerHTML = numberOfExternalIPs;
         document.getElementById('workers').innerHTML = nubmerOfWorkers;
         document.getElementById('man').innerHTML = numberOfManagement;
-	});	
-	reader.readAsText(file);
-	
-	var fullPath = document.getElementById('file-input').value;
-	if (fullPath) {
-		var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
-		var filename = fullPath.substring(startIndex);
-		if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
-			filename = filename.substring(1);
-		}
-        document.getElementById("path").innerHTML = "$Home/"+filename;
+    });
+    reader.readAsText(file);
+
+    var fullPath = document.getElementById('file-input').value;
+    if (fullPath) {
+        var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+        var filename = fullPath.substring(startIndex);
+        if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+            filename = filename.substring(1);
+        }
+        document.getElementById("path").innerHTML = "$Home/" + filename;
     }
 }
 
@@ -78,7 +80,7 @@ var match = /^data:(.*);base64,(.*)$/.exec(e.target.result);
 		alert(mimeType);
 		alert(content);
 	}
-} */ 
+} */
 
 /* $(function () {
     $('#import-pfx-button').click(function(e) {
@@ -87,8 +89,8 @@ var match = /^data:(.*);base64,(.*)$/.exec(e.target.result);
     $('#file-input').change(handleFileSelect);
 }); */
 
-$(function () {
-	
+$(function() {
+
     $('#file-input').click();
 
     $('#file-input').change(handleFileSelect);
