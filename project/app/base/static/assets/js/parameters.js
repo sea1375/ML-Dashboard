276be712581e5c65865ba9f8b1baa37fe128aa01
Sea1375 <sea.dream0000@gmail.com>
2021-02-05 16:06:21 -0800

    new project saveFile

--- project/app/base/static/assets/js/parameters.js
+++ project/app/base/static/assets/js/parameters.js
@@ -6,22 +6,16 @@ function saveFile(projs,data) {
 	
 	/*alert(projs);*/
 	const nameVal = document.getElementById("project").value;
-	const prefixVal = document.getElementById("prefix").value;
-	const trainVal = data+"\\"+document.getElementById("train").value;
 	const modeVal = document.getElementById("trainingmode").value;
 	const algoVal = document.getElementById("algorithm").value;
-	const rateVal = document.getElementById("scale1").value;
 	const epochsVal = document.getElementById("epochs").value;
 	const perVal = document.getElementById("scale2").value;	
 	const degVal = document.getElementById("scale3").value;
 	const maxVal = document.getElementById("scale4").value;
 	let project = { 
 		name: nameVal,
-		prefix: prefixVal, 
-		path: trainVal,
 		mode: modeVal,
 		algorithm: algoVal,
-		rate: rateVal,
 		epochs: epochsVal,
 		percentage: perVal,
 		max_degree: degVal,
@@ -31,7 +25,7 @@ function saveFile(projs,data) {
 	let d = JSON.stringify(project); 
 	/* alert(d);*/
 	const textToBLOB = new Blob([d], { type: 'text/plain' });
-	const sFileName = data+"//"+nameVal+".json";	   // The file to save the data.
+	const sFileName = nameVal+".json";	   // The file to save the data.
 
 	let newLink = document.createElement("a");
 	newLink.download = sFileName;
